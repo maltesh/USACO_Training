541329a9698c20302a60303c822c47edba75a3b8
Maltesh <malteshgm@blinkx.com>
2015-09-07 17:00:31 +0530

    Cleanups

--- src/practice/Gift1.java
+++ src/practice/Gift1.java
@@ -6,7 +6,11 @@ TASK: gift1
 package practice;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
@@ -15,8 +19,9 @@ public class Gift1 {
     private static HashMap<String,Integer> giftGiven = new HashMap<String,Integer>();
     private static HashMap<String,Integer> giftsReceived = new HashMap<String,Integer>();
     private static HashMap<String,Integer> balanceLeftOver = new HashMap<String,Integer>();
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         List<String> allMembers  = new ArrayList<String>();
+        PrintWriter out  = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\sg1852\\Desktop\\test.out")));
         try
         {
             BufferedReader file = new BufferedReader(new FileReader(filePath));
@@ -31,17 +36,14 @@ public class Gift1 {
             while((currentLine=file.readLine())!=null)
             {
                 String name = currentLine;
-                System.out.println(name.split(",").length);
                 if(name.split(",").length <= 1)
                 {
-                    
                     String nextLine       = file.readLine();
                     String [] nos         = nextLine.split(" ");
                     int totalPplReceiving = Integer.parseInt ( nos[1] );
                     int totalAmount       = Integer.parseInt(nos[0]);
                     giftGiven.put(name, totalAmount);
                     int amountReceived    =  (totalPplReceiving>0)?totalAmount/totalPplReceiving:0;
-                    System.out.println(amountReceived);
                     balanceLeftOver.put(name, totalAmount-amountReceived*totalPplReceiving);
                     for(int j =0 ;j<totalPplReceiving ; j++)
                     {
@@ -54,25 +56,19 @@ public class Gift1 {
                     }
                 }
             }
-            System.out.println(giftGiven);
-            System.out.println(giftsReceived);
-            System.out.println(balanceLeftOver);
-            
             for(String str :allMembers)
             {
                 int gvn  = ( giftGiven.containsKey(str) )? giftGiven.get(str):0;
                 int rcvd = ( giftsReceived.containsKey(str) )? giftsReceived.get(str):0;
                 int blnc = ( balanceLeftOver.containsKey(str) )? balanceLeftOver.get(str):0;
                 int leftOver = rcvd+blnc-gvn;
-                System.out.println(str + " " + leftOver );
+                out.println(leftOver);
             }
+            out.close();
             System.exit(0);
         }
         catch(Exception e)
         {
-            System.out.println(" Exception");
-            System.out.println(e.getMessage());
         }
-        
     }
 }
